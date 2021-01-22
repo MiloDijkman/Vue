6abe97295bb24a4fc4a8f0d0c0c4f58fe23ba63d
MiloDijkman <55231114+MiloDijkman@users.noreply.github.com>
2021-01-22 12:38:25 +0100

    Add files via upload

---
+++ Boodschappen.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div v-bind:key="boodschappen.id" v-for="boodschappen in boodschappen">
+      <Boodschappen v-bind:boodschappen="boodschappen" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BoodschappenItem from './boodschappenlijst.vue';
+
+export default {
+  name: "Boodschappen",
+  components: {
+    BoodschappenItem
+  },
+  props: ["boodschappen"]
+}
+</script>
+
+<style scoped>
+
+</style>
